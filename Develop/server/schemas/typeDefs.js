6c935b959555dbc04fe8acd5d334ac4abc55aa7e
Floki <ypr_tylerkeuler@aol.com>
2022-06-26 22:15:00 -0400

    added logic to get apollo server running and graphQl

---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,47 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  input BookInput {
+    authors: [String]
+    description: String
+    bookId: ID!
+    title: String
+    image: String
+    link: String
+  }
+
+  type Book {
+    authors: [String]
+    description: String!
+    bookId: ID!
+    image: String
+    link: String
+    title: String
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String): Auth
+    loginUser(email: String!, password: String!): Auth
+    saveBook(bookData: BookInput): User
+    removeBook(bookId: ID): User
+  }
+`;
+
+module.exports = typeDefs;
